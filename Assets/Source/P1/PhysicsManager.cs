b98f28926806e63e1b53cf52009ee0fb200789a4
Andremi <andremiguelperez@gmail.com>
2026-02-01 16:01:02 +0100

    Initial commit

---
+++ Assets/Source/P1/PhysicsManager.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using VectorXD = MathNet.Numerics.LinearAlgebra.Vector<double>;
+using MatrixXD = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+using DenseVectorXD = MathNet.Numerics.LinearAlgebra.Double.DenseVector;
+using DenseMatrixXD = MathNet.Numerics.LinearAlgebra.Double.DenseMatrix;
+
+/// <summary>
+/// Basic physics manager capable of simulating a given ISimulable
+/// implementation using diverse integration methods: explicit,
+/// implicit, Verlet and semi-implicit.
+/// </summary>
+public class PhysicsManager : MonoBehaviour 
+{
+	/// <summary>
+	/// Default constructor. Zero all. 
+	/// </summary>
+	public PhysicsManager()
+	{
+		Paused = true;
+		TimeStep = 0.01f;
+		Gravity = new Vector3 (0.0f, -9.81f, 0.0f);
+		IntegrationMethod = Integration.Explicit;
+	}
+
+	/// <summary>
+	/// Integration method.
+	/// </summary>
+	public enum Integration
+	{
+		Explicit = 0,
+		Symplectic = 1,
+        Midpoint = 2,
+        Verlet = 3,
+        Implicit = 4,
+	};
+
+	#region InEditorVariables
+
+	public bool Paused;
+	public float TimeStep;
+    public Vector3 Gravity;
+    public List<GameObject> SimObjects;
+    public Integration IntegrationMethod;
+
+    #endregion
+
+    #region OtherVariables
+    private List<ISimulable> m_objs;
+    private int m_numDoFs;
+    #endregion
+
+    #region MonoBehaviour
+
+    public void Start()
+    {
+        //Parse the simulable objects and initialize their state indices
+        m_numDoFs = 0;
+        m_objs = new List<ISimulable>(SimObjects.Capacity);
+
+        foreach (GameObject obj in SimObjects)
+        {
+            ISimulable simobj = obj.GetComponent<ISimulable>();
+            if (simobj != null)
+            {
+                m_objs.Add(simobj);
+
+                // Initialize simulable object
+                simobj.Initialize(m_numDoFs, this);
+
+                // Retrieve pos and vel size
+                m_numDoFs += simobj.GetNumDoFs();
+            }
+        }
+    }
+
+    public void Update()
+	{
+		if (Input.GetKeyUp (KeyCode.P))
+			this.Paused = !this.Paused;
+
+    }
+
+    public void FixedUpdate()
+    {
+        if (this.Paused)
+            return; // Not simulating
+
+        // Select integration method
+        switch (this.IntegrationMethod)
+        {
+            case Integration.Explicit: this.stepExplicit(); break;
+            case Integration.Symplectic: this.stepSymplectic(); break;
+            case Integration.Midpoint: this.stepMidpoint(); break;
+            case Integration.Verlet: this.stepVerlet(); break;
+            case Integration.Implicit: this.stepImplicit(); break;
+            default:
+                throw new System.Exception("[ERROR] Should never happen!");
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Performs a simulation step using Explicit integration.
+    /// </summary>
+    private void stepExplicit()
+	{
+        VectorXD x = new DenseVectorXD(m_numDoFs);
+        VectorXD v = new DenseVectorXD(m_numDoFs);
+        VectorXD f = new DenseVectorXD(m_numDoFs);
+        f.Clear();
+        MatrixXD Minv = new DenseMatrixXD(m_numDoFs);
+        Minv.Clear();
+
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.GetPosition(x);
+            obj.GetVelocity(v);
+            obj.GetForce(f);
+            obj.GetMassInverse(Minv);
+        }
+
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.FixVector(f);
+            obj.FixMatrix(Minv);
+        }
+
+        x += TimeStep * v;
+        v += TimeStep * (Minv * f);
+
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.SetPosition(x);
+            obj.SetVelocity(v);
+        }
+    }
+
+    /// <summary>
+    /// Performs a simulation step using Symplectic integration.
+    /// </summary>
+    private void stepSymplectic()
+	{
+        VectorXD x = new DenseVectorXD(m_numDoFs);
+        VectorXD v = new DenseVectorXD(m_numDoFs);
+        VectorXD f = new DenseVectorXD(m_numDoFs);
+        f.Clear();
+        MatrixXD Minv = new DenseMatrixXD(m_numDoFs);   // Inverse of the mass
+        Minv.Clear();
+
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.GetPosition(x);
+            obj.GetVelocity(v);
+            obj.GetForce(f);
+            obj.GetMassInverse(Minv);
+        }
+
+        // Fix the nodes that dont move
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.FixVector(f);
+            obj.FixMatrix(Minv);
+        }
+
+        // TimeStep: symplectic euler
+        x += TimeStep * v;
+        v += TimeStep * (Minv * f);
+
+
+        // Store the new values
+        foreach (ISimulable obj in m_objs)
+        {
+            obj.SetPosition(x);
+            obj.SetVelocity(v);
+        }
+    }
+
+    /// <summary>
+    /// Performs a simulation step using Midpoint integration.
+    /// </summary>
+    private void stepMidpoint()
+    {
+        // TO BE COMPLETED //
+    }
+
+    /// <summary>
+    /// Performs a simulation step using Verlet integration.
+    /// </summary>
+    private void stepVerlet()
+    {
+        // TO BE COMPLETED //
+    }
+
+    /// <summary>
+    /// Performs a simulation step using Implicit integration.
+    /// </summary>
+    private void stepImplicit()
+    {
+        // TO BE COMPLETED //
+    }
+
+}
---
+++ Assets/Source/P1/Spring.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VectorXD = MathNet.Numerics.LinearAlgebra.Vector<double>;
+using MatrixXD = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+using DenseVectorXD = MathNet.Numerics.LinearAlgebra.Double.DenseVector;
+using DenseMatrixXD = MathNet.Numerics.LinearAlgebra.Double.DenseMatrix;
+
+public class Spring : MonoBehaviour {
+
+    #region InEditorVariables
+
+    public float Stiffness;
+    public Node nodeA;
+    public Node nodeB;
+
+    #endregion
+
+    public float Length0;
+    public float Length;
+
+    private PhysicsManager Manager;
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 yaxis = new Vector3(0.0f, 1.0f, 0.0f);
+        Vector3 dir = nodeA.Pos - nodeB.Pos;
+        dir.Normalize();
+
+        transform.position = 0.5f * (nodeA.Pos + nodeB.Pos);
+        //The default length of a cylinder in Unity is 2.0
+        transform.localScale = new Vector3(transform.localScale.x, Length / 2.0f, transform.localScale.z);
+        transform.rotation = Quaternion.FromToRotation(yaxis, dir);
+	}
+
+    // Use this for initialization
+    public void Initialize(PhysicsManager m)
+    {
+        Manager = m;
+
+        UpdateState();
+        Length0 = Length;
+    }
+
+    // Update spring state
+    public void UpdateState()
+    {
+        Length = (nodeA.Pos - nodeB.Pos).magnitude;
+    }
+
+    // Get Force
+    public void GetForce(VectorXD force)
+    {
+        Vector3 hooke = - Stiffness * (Length - Length0) * (nodeA.Pos - nodeB.Pos) / Length;
+        force[nodeA.index] += hooke[0];
+        force[nodeA.index + 1] += hooke[1];
+        force[nodeA.index + 2] += hooke[2];
+        force[nodeB.index] -= hooke[0];
+        force[nodeB.index + 1] -= hooke[1];
+        force[nodeB.index + 2] -= hooke[2];
+    }
+
+    // Get Force Jacobian
+    public void GetForceJacobian(MatrixXD dFdx)
+    {
+        // TO BE COMPLETED //
+    }
+
+}
